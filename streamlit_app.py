154f18758fb3b8d85196e441a53a5df346675a21
moonbuggy42 <mihin0011@gmail.com>
2026-05-09 21:38:48 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,11 +19,11 @@ with st.sidebar:
   elif num_outcomes == 4:
     out_1 = st.selectbox("Enter Outcome 1", ("Excellent", "High", "Standard", "Partial", "Limited"))
     out_2 = st.selectbox("Enter Outcome 2", ("Excellent", "High", "Standard", "Partial", "Limited"))
-    out_3 = st.selectbox("Enter Outcome 3", ("Excellent", "High", "Standard", "Partial", "Limited")
-    out_4 = st.selectbox("Enter Outcome 4", ("Excellent", "High", "Standard", "Partial", "Limited")
+    out_3 = st.selectbox("Enter Outcome 3", ("Excellent", "High", "Standard", "Partial", "Limited"))
+    out_4 = st.selectbox("Enter Outcome 4", ("Excellent", "High", "Standard", "Partial", "Limited"))
   else:
     out_1 = st.selectbox("Enter Outcome 1", ("Excellent", "High", "Standard", "Partial", "Limited"))
     out_2 = st.selectbox("Enter Outcome 2", ("Excellent", "High", "Standard", "Partial", "Limited"))
-    out_3 = st.selectbox("Enter Outcome 3", ("Excellent", "High", "Standard", "Partial", "Limited")
+    out_3 = st.selectbox("Enter Outcome 3", ("Excellent", "High", "Standard", "Partial", "Limited"))
     out_4 = st.selectbox("Enter Outcome 4", ("Excellent", "High", "Standard", "Partial", "Limited"))
-    out_5 = st.selectbox("Enter Outcome 5", ("Excellent", "High", "Standard", "Partial", "Limited")
+    out_5 = st.selectbox("Enter Outcome 5", ("Excellent", "High", "Standard", "Partial", "Limited"))
