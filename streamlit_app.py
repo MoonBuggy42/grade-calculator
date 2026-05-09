3b5b650af8d564d843961670aadb05ed5c1d0806
moonbuggy42 <mihin0011@gmail.com>
2026-05-09 21:31:05 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import plotly.express as px
 import numpy as np
 
 st.title('🥇Grade Calculator')
-with st.sidebar():
+with st.sidebar:
   st.subheader("Previous Outcomes")
   num_outcomes = st.slider("Number of Outcomes in Semester", 1, 10, 5)
   for x in range(num_outcomes):
